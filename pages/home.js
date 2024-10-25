92d05368e6a844c9195c4c00c4614fb20857a85c
MINJIA <mi1224ke06@gmail.com>
2024-10-25 09:01:24 +0800

    page-home

---
+++ pages/home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Nav from '@/components/nav'
+import Footer from '@/components/footer'
+import Commodity1 from '@/components/card/ commodity'
+import Category from '@/components/card/category'
+export default function home() {
+  return (
+    <div>
+       <Nav />
+       <Commodity1 />
+
+       <Category />
+       <Footer />
+       <style jsx>
+          {`
+           
+          `}
+       </style>
+    </div>
+
+  )
+}
