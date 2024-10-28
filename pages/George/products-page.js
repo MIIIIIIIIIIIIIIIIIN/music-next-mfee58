687d830dfed4c11f649cbb3e1ed99140189e15ed
goerg9523 <goerg9523@gmail.com>
2024-10-28 21:53:29 +0800

    editing products-page

--- pages/George/products-page.js
+++ pages/George/products-page.js
@@ -3,10 +3,13 @@ import ProductsLatestLaunched from '@/components/George/products-mall/products-l
 import Footer from '@/components/public/footer';
 import ProductsRecommendation from '@/components/George/products-mall/products-recommendation';
 import ProductsActivities from '@/components/George/products-mall/products-activities';
+import Forum from '@/components/public/nav/checklist/forum';
+import NavDesktop from '@/components/public/nav/desktop';
 
 export default function ProductsPage() {
   return (
     <>
+    <NavDesktop />
     <ProductsActivities/>
     <ProductsRecommendation />
     <ProductsLatestLaunched />
