70c33de3bb8142c9c8a6279251260bbf9efe2413
Mike <mi1224ke06@gmail.com>
2024-10-28 10:48:56 +0800

    測試

--- pages/home.js
+++ pages/home.js
@@ -10,8 +10,8 @@ import Carousel from '@/components/public/Carousel'
 
 export default function home() {
   const [isMobile, setIsMobile] = useState(false);
-  const images = [ "/image/001album.jpg", "/image/002album.jpg",
-    "/image/003album.jpg", "/image/004album.jpg", "/image/005album.jpg", ];
+  const images = [ "/01.jpg", "/02.jpg",
+    "/03.jpg", ];
     
   // const [isMobile, setIsMobile] = useState(false);
   useEffect(() => {
