4aca82dfaac35cba186be25ed923feca43ea0131
Jade <luaraitw@gmail.com>
2024-11-01 14:49:20 +0800

    暫存 web-2

--- components/member/mem-web-2/index.js
+++ components/member/mem-web-2/index.js
@@ -1,14 +1,36 @@
 import React from "react";
 import styles from "./mem-web-2.module.css";
 import Nav from "@/components/public/nav";
+import MemIcons from "../mem-icons";
+import UserIcon from "@/components/public/user-icon";
+import InfoNav from "../info-nav";
 
 const MemWeb2 = () => {
   return (
     <>
-      <div className={styles.wrap}>
-        <div className={styles.container}>
-          123
+      <div className={styles["mem-web"]}>
+        <Nav />
+      </div>
+
+      <div className={styles["container"]}>
+        <div className={styles["sec1"]}>
+          <div className="info">
+            <InfoNav />
+          </div>
+          <div className="main">
+            <MemIcons iconName="icon-mail" />
+            <UserIcon />
+            <UserIcon />
+            <UserIcon />
+            <UserIcon />
+            <UserIcon />
+            <UserIcon />
+            <UserIcon />
+            <UserIcon />
+            <UserIcon />
+          </div>
         </div>
+        <div className={styles["footer1"]}></div>
       </div>
     </>
   );
