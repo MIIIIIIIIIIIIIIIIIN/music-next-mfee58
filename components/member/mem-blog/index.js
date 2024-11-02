b4e66471ecdab990b93e5e360dbc68478caf826a
Jade <luaraitw@gmail.com>
2024-11-03 02:19:08 +0800

    建立 member-Blog 頁面

--- components/member/mem-blog/index.js
+++ components/member/mem-blog/index.js
@@ -1,37 +1,50 @@
-import React from 'react';
-import styles from './mem-blog.module.css'; // 引入相應的 CSS 模組
-import Nav from '@/components/public/nav';
-import UserIcon from '@/components/public/user-icon';
-import BlogNav from '../blog-nav';
-
+import React from "react";
+import styles from "./mem-blog.module.css"; // 引入相應的 CSS 模組
+import Nav from "@/components/public/nav";
+import UserIcon from "@/components/public/user-icon";
+import BlogNav from "../blog-nav";
+import PlayButton from "@/components/public/play-button";
 
 const MemBlog = () => {
   return (
     <>
-    <div className={styles["wrapper"]}>
-      <Nav className={styles["nav"]} />
+      <div className={styles["wrapper"]}>
+        <Nav className={styles["nav"]} />
 
-      <div className={styles["container"]}>
-        <div className={styles["leftContent"]}>
-          {/* <InfoNav /> */}
-      <BlogNav />
+        <div className={styles["container"]}>
+          <div className={styles["leftContent"]}>
+            {/* <InfoNav /> */}
+            <BlogNav />
+          </div>
+          <div className={styles["rightContent"]}>
+            <div className={styles["album"]}>
+              <div className="icon"></div>
+              <UserIcon />
+            </div>
+            
+            <div className={styles["album"]}>
+              <PlayButton size="large" />
+            </div>
+            <div className={styles["album"]}>album</div>
+            <div className={styles["album"]}>album</div>
+            <div className={styles["album"]}>album</div>
+            <div className={styles["album"]}>album</div>
+            <div className={styles["album"]}>album</div>
+            <div className={styles["album"]}>album</div>
+            <div className={styles["album"]}>album</div>
+            <div className={styles["album"]}>album</div>
 
-
+            <UserIcon />
+            <UserIcon />
+            <UserIcon />
+            <UserIcon />
+            <UserIcon />
+          </div>
         </div>
-        <div className={styles["rightContent"]}>
-      <UserIcon />
-      <UserIcon />
-      <UserIcon />
-      <UserIcon />
-      <UserIcon />
-      <UserIcon />
-      <UserIcon />
-        </div>
+
+        <div className={styles["footer"]}>Footer</div>
       </div>
-
-      <div className={styles["footer"]}>Footer</div>
-    </div>
-  </>
+    </>
   );
 };
 
