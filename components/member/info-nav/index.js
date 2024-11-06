18004de9023cf7d7e30416bbb754d7c7edaeb1db
Jade <luaraitw@gmail.com>
2024-11-06 15:02:00 +0800

    新增 onClick 功能

--- components/member/info-nav/index.js
+++ components/member/info-nav/index.js
@@ -4,7 +4,7 @@ import styles from './info-nav.module.css';
 const InfoNav = ({ onScrollToInfo, onScrollToAcc, onScrollToPassword, onScrollToFav, onScrollToUploader }) => {
   return (
     <div className={styles["info-nav"]}>
-      <h4 className={styles["nav-title"]}>會員中心</h4>
+      <h4 className={styles["nav-title"]}onClick={onScrollToInfo}>會員中心</h4>
       <div className={styles["nav-body"]}>
         <h5 className={styles["body-text"]} onClick={onScrollToInfo}>基本資料</h5>
         <h5 className={styles["body-text"]} onClick={onScrollToAcc}>帳號設定</h5>
