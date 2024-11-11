d185f4f33c12edfe0e00731e1abd2edf8e8f7ec3
FrostAce52 <ezioallen@gmail.com>
2024-11-11 15:50:28 +0800

    add loading screen component

--- pages/Allen/forum.js
+++ pages/Allen/forum.js
@@ -1,20 +1,34 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import ForumContainer from "@/components/forum/forum";
 import NavBar from "@/components/public/nav";
 import { Fade } from "react-awesome-reveal";
 import { AnimatedBackground } from "animated-backgrounds";
+import Loading from "@/components/forum/loading";
 
-export default function test() {
+export default function Forum() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Set to 2.5s (2100ms + 400ms buffer) as that's when the last animation finishes
+    const timer = setTimeout(() => setLoading(false), 2500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
-    <>
+    <div className="min-h-screen">
       <NavBar />
       <AnimatedBackground
         animationName="geometricShapes"
         style={{ opacity: 0.5 }}
       />
-      <Fade delay={1e2} cascade damping={1e-1}>
+      <Fade delay={100} cascade damping={0.1}>
         <ForumContainer />
       </Fade>
-    </>
+    </div>
   );
 }
